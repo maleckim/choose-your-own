a3b00b500362cc852f399fbcdb88f8bc155c1d48
maleckim <maleckimatthewm@gmail.com>
2023-08-29 13:10:45 -0700

    merge

---
+++ src/components/StagePresenter.js
@@ -0,0 +1,23 @@
+import React from "react";
+import CharacterStage from "./CharacterStage";
+import FirstAct from "../dialogue/FirstAct"
+
+const StagePresenter = ({character}) => {
+
+    const DetermineAct = () => {
+
+        return (
+            <FirstAct />
+        )
+    }
+
+
+
+    return(
+        <CharacterStage character={character} Act={DetermineAct} />
+    )
+
+
+}
+
+export default StagePresenter
