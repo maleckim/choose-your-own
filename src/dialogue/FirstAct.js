796771bd10236118114fbee7a3727d99676daa38
Matthew Malecki <maleckimatthewm@gmail.com>
2023-08-30 12:34:56 -0500

    none

--- src/dialogue/FirstAct.js
+++ src/dialogue/FirstAct.js
@@ -4,7 +4,7 @@ const FirstAct = () => {
 
     const [dialogueCount, setDialogueCount] = useState(0);
     const [intro, setIntro] = useState(true);
-    const [userSpeaking, setUserSpeaking] = useState(false);
+    const [convoCount, setConvoCount] = useState();
     const [npcSpeaking, setNpcSpeaking] = useState(true)
     const [userDialogueCount, setUserDialogueCount] = useState(0);
 
@@ -15,15 +15,23 @@ const FirstAct = () => {
             "Ah, the allure of those slender sticks of contemplation, those cylindrical companions of mine, the cigarettes! Their embered tips whisper secrets to the night, casting a subtle haze that tangles reality with introspection. Oh, the dance of smoke, a dance of paradoxes, as it unfurls tendrils of both freedom and captivity. The way they nestle between my fingers, a bridge between worlds, and how they burn with a fervor that mirrors the fires of my ruminations. Yes, dear interlocutors, you might raise your eyebrows, but to me, these paper-clad wonders are an ephemeral escape, a paradoxical path to both solace and introspection."
         ],
         userResponses: {
-            0: ['1', '2', '3'],
+            0: ['cool', 'rude', 'weird'],
             1: ['3', '3', '4']
+        },
+        npcDialogueFirst:{
+            0: 'cool',
+            1: 'rude',
+            2: 'weird'
         }
     }
+
+
 
     const renderIntro = () => {
         if (dialogueCount >= 3) {
             if (intro === true) {
                 setIntro(false)
+                setConvoCount(convoCount + 1)
             }
             setNpcSpeaking(false)
             setDialogueCount(0)
@@ -34,8 +42,13 @@ const FirstAct = () => {
         )
     }
 
-    const incrementUserDialogue = () => {
+    const renderFirstResponse = () => {
+        
+    }
+
+    const userDialogueHandler = (e) => {
         //increment dialogue and switch speaker
+        console.log(e.target.value)
         setUserDialogueCount(userDialogueCount + 1)
         setNpcSpeaking(true)
     }
@@ -44,7 +57,7 @@ const FirstAct = () => {
         const test = Object.values(dialogue.userResponses[userDialogueCount])
         return (
 
-            <p>{test.map(a => { return <button onClick={() => incrementUserDialogue() }>{a}</button> })}</p>
+            <p>{test.map(a => { return <button value={a} onClick={(e) => userDialogueHandler(e) }>{a}</button> })}</p>
 
         )
     }
